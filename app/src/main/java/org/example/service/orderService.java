9ba9328bb5dc213ca480b313877def4254e9e86a
atharsh07 <atharsh0425@gmail.com>
2025-05-16 13:18:56 +0530

    orderServices

---
+++ app/src/main/java/org/example/service/orderService.java
@@ -0,0 +1,11 @@
+package org.example.service;
+
+import org.example.bean.UserConfig;
+
+public class orderService {
+
+    public static orderService createInstance(UserConfig userConfig, UserService userService){
+        return null;
+    }
+
+}
